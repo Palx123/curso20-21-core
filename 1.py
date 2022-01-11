c8eb69d2976eee5c5b06f8bb22794ba7823d4fed
Palx123 <94451604+Palx123@users.noreply.github.com>
2022-01-11 18:51:46 +0100

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,48 @@
+import os
+os.system('clear')
+
+"""
+Son funciones que admiten funciones como parámetros
+y devuelven funciones
+"""
+def mi_decorador(funcion_a_decorar):
+    def envoltorio(args,**kwargs):
+        print('------------ Empiezo a Decorar')
+        c = funcion_a_decorar(args,*kwargs)
+        print(c)
+        print('-------------Fin de la decoración')
+
+        return c
+    return envoltorio
+
+@mi_decorador
+def saludar():
+    print('Hola Mundo!!')
+
+
+def decora_sumar(funcion_que_suma):
+    def identifica(args):
+        if not type(args) == tuple:
+            lista = tuple(args)
+            return funcion_que_suma(lista)
+        else:
+            return funcion_que_suma(args)
+    return identifica
+
+
+#saludar()
+
+# resp = mi_decorador(saludar)
+# resp()
+@decora_sumar
+def sumar(lista1):
+    lista1 = []
+    for i in lista1:
+    if type(i) == tuple:
+        suma = decora_sumar
+    return sumar([lista1])
+
+# def restar(a,b):
+#     return a - b
+
+print(sumar([1,2,3,4,5,6]))
